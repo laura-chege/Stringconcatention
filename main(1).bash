0a6332311c88cb0ba55541222bbb123245e80e13
Laurah <76225426+laura-chege@users.noreply.github.com>
2021-11-18 13:01:09 +0300

    Update main(1).bash

--- main(1).bash
+++ main(1).bash
@@ -1,6 +1,3 @@
-#                             Online Bash Shell.
-#                 Code, Compile, Run and Debug Bash script online.
-# Write your code in this editor and press "Run" button to execute it.
 
 echo "Enter the first string:" 
 read s1 
